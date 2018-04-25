30644d08c001d28ffcb35ff420a5dd829ab461c2
Jake Willson <jakedwillson@ksu.edu>
2018-04-24 19:36:51 -0500

    failed_one

--- Ksu.Cis300.NetworkFlow/Form1.Designer.cs
+++ Ksu.Cis300.NetworkFlow/Form1.Designer.cs
@@ -28,9 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 350);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.True);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Ksu.Cis300.NetworkFlow/Form1.cs
+++ Ksu.Cis300.NetworkFlow/Form1.cs
@@ -16,5 +16,14 @@ namespace Ksu.Cis300.NetworkFlow
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void True(object sender, MouseEventArgs e)
+        {
+        }
     }
 }
